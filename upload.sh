5d313aa3f42ee7848bfec40e481dc4df146bdb49
leiwenxuan <892028617@qq.com>
2020-02-01 17:06:44 +0800

    推送GitHub

--- upload.sh
+++ upload.sh
@@ -1,6 +1,4 @@
 git add .
 git commit -m "推送GitHub"
-# git remote add origin https://github.com/leiwenxuan/crontab.git
+git remote add origin https://github.com/leiwenxuan/lwx-crontab.git
 git push -u origin master
-
-
